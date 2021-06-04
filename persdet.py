bd2679e32d1d5f4cb5c355eaf338f75be641c0d5
fope-law <fope1@live.com>
2021-06-04 14:40:48 +0000

    Project 1

---
+++ persdet.py
@@ -0,0 +1,15 @@
+name = "Fopé"
+
+age = 28
+
+first_name = "First Name"
+
+last_name = "Last Name"
+
+print(name + " is " + str(age)) 
+
+print("Hello World")
+
+print(first_name + "" ": Fopé")
+print(last_name + "" ": Law")
+
