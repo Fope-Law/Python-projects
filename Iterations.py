0c2bb29eda95ddf019b105daae7fb94456e3fd5f
fope-law <fope1@live.com>
2021-06-06 15:38:13 +0000

    Overview of iterations

--- Iterations.py
+++ Iterations.py
@@ -5,5 +5,28 @@ while count < 5:
     print(name, "is awesome!")
     count += 1
 
-for me in range(5,11):
+for me in range(51):
     print(me)
+
+for me in range(0, 51, 2):
+    print(me)
+
+animals = ["bull", "boar", "swan"]
+
+for animal in animals:
+    print(animal)
+
+for me in range(101):
+    print(me)
+    if me == 20:
+        break
+       
+for me in range(20):
+    if me == 5:
+        continue
+    print(me) 
+
+for i in range(3):
+    for j in range(4):
+        print(i, "x", j, "=", i*j)
+
