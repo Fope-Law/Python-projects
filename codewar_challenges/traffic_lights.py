9304a621689cd92ccdf7fe81e527fab15342d0d7
Fope-Law <fope1@live.com>
2021-06-11 00:32:53 +0000

    Code Wars challenge

---
+++ codewar_challenges/traffic_lights.py
@@ -0,0 +1,13 @@
+def update_light(current):
+    if current == "green":
+        return "yellow"
+    elif current == "yellow":
+        return "red"
+    elif current == "red":
+        return "green"
+    else:
+        return "This is not a traffic light color."
+
+print(update_light("red"))
+print(update_light("yellow"))
+print(update_light("green"))
